682755e1f2d9bf745230f500a85397101f7f78a7
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-17 14:55:00 -0500

    #3 configuration object

--- EASConfiguration.cs
+++ EASConfiguration.cs
@@ -6,11 +6,26 @@ namespace EncryptionAPIServicesSDK
 {
     public static class EASConfiguration
     {
+        /// <summary>
+        /// The consumer of the library should be able to set in their API Token as such
+        /// EASConfiguration.ApiToken = "MyApiToken";
+        /// </summary>
         private static string _ApiToken;
         public static string ApiToken
         {
             get { return _ApiToken; }
             set { _ApiToken = value; }
+        }
+
+        /// <summary>
+        /// The Base URL should automatically point to the production server api route. However,
+        /// the user should be able to override it for local testing purposes.
+        /// </summary>
+        private static string _BaseUrl = "https://encryptionapiservices.com/api/";
+        public static string BaseUrl
+        {
+            get { return _BaseUrl; }
+            set { _BaseUrl = value; }
         }
     }
 }
