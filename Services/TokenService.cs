0b2cfc57c91d26355c336b4143247f5f3902a04f
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-18 10:41:28 -0500

    #10 creating method in token service to get refresh token

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -15,9 +15,11 @@ namespace EncryptionAPIServicesSDK.Services
         }
 
         /// <summary>
-        /// Calls the TokenController in the main API to retrieve the token by the user's api key.
+        /// Calls the TokenController in the main API to retrieve the token by the user's api key
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Returns an empty string if there was an error on the API side. 
+        /// If successful returns the token in encoded string format
+        /// </returns>
         public async Task<string> GetToken()
         {
             string token = string.Empty;
@@ -25,11 +27,46 @@ namespace EncryptionAPIServicesSDK.Services
             {
                 throw new Exception("Please set your ApiKey provided in your dashboard to the EASConfiguration object.");
             }
-            string url = EASConfiguration.BaseUrl + String.Format("token");
-            this._httpClient.DefaultRequestHeaders.Add("ApiKey", EASConfiguration.ApiKey);
+            string url = EASConfiguration.BaseUrl + "token";
+            if (!this._httpClient.DefaultRequestHeaders.Contains("ApiKey"))
+            {
+                this._httpClient.DefaultRequestHeaders.Add("ApiKey", EASConfiguration.ApiKey);
+            }
             HttpResponseMessage response = await this._httpClient.GetAsync(url);
             GetTokenResponse responseBody = JsonConvert.DeserializeObject<GetTokenResponse>(await response.Content.ReadAsStringAsync());
-            return responseBody.Token;
+            token = responseBody.Token;
+            return token;
+        }
+
+        /// <summary>
+        /// User passes in a token and calls the main API to create a new token if it expired.
+        /// Returns the same token if the token is still valid.
+        /// </summary>
+        /// <returns>Returns an empty string if there was an error on the API side. 
+        /// If successful returns the token in encoded string format.
+        /// </returns>
+        public async Task<string> GetRefreshToken(string oldToken)
+        {
+            string token = string.Empty;
+            if (string.IsNullOrEmpty(EASConfiguration.ApiKey))
+            {
+                throw new Exception("Please set your ApiKey provided in your dashboard to the EASConfiguration object.");
+            }
+            if (string.IsNullOrEmpty(oldToken))
+            {
+                throw new Exception("Please pass in an expired token to replace the existing token");
+            }
+            string url = EASConfiguration.BaseUrl + "token/RefreshToken";
+            if (!this._httpClient.DefaultRequestHeaders.Contains("ApiKey"))
+            {
+                this._httpClient.DefaultRequestHeaders.Add("ApiKey", EASConfiguration.ApiKey);
+
+            }
+            this._httpClient.DefaultRequestHeaders.Add("Authorization", String.Format("Bearer {0}", oldToken));
+            HttpResponseMessage response = await this._httpClient.GetAsync(url);
+            GetRefreshTokenResponse responseBody = JsonConvert.DeserializeObject<GetRefreshTokenResponse>(await response.Content.ReadAsStringAsync());
+            token = responseBody.Token;
+            return token;
         }
     }
 }
