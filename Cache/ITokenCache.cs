6d01e616c530255e6ac0f067a4a299e4b10b1eb8
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-20 22:34:07 -0500

    #18 token cache

---
+++ Cache/ITokenCache.cs
@@ -0,0 +1,18 @@
+﻿using EncryptionAPIServicesSDK.Services;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
+
+namespace EncryptionAPIServicesSDK.Cache
+{
+    public interface ITokenCache
+    {
+        string token { get; set; }
+        DateTime expirationTime { get; set; }
+
+        TokenService _tokenService { get; set; }
+        Task<string> GetToken();
+        Task SetCache();
+        void SetDate(JwtSecurityToken token);
+    }
+}
